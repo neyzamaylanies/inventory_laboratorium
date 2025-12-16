7ad869e60cf971cf79a9e92fbc1b54ff90731325
neyzamaylanies <neyzamayylanies@gmail.com>
2025-12-16 13:47:28 +0700

    added new file

---
+++ src/main/java/project4_3fsd2/inventory_laboratorium/InventoryLaboratoriumApplication.java
@@ -0,0 +1,13 @@
+package project4_3fsd2.inventory_laboratorium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryLaboratoriumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryLaboratoriumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project4_3fsd2/inventory_laboratorium/InventoryLaboratoriumApplicationTests.java
@@ -0,0 +1,13 @@
+package project4_3fsd2.inventory_laboratorium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryLaboratoriumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
